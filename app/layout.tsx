a41883ce65e4a8f5b0ae2d17ada31e7e6c8ac4ac
Alan Bruno Ferreira da Silva <alan.ferreira@pixter.com.br>
2023-08-01 00:18:00 -0300

    feat: implementation of the navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,8 @@ import { Inter } from 'next/font/google'
 import { ClerkProvider } from '@clerk/nextjs'
 import { neobrutalism } from '@clerk/themes'
 
+import { ThemeProvider } from '@/components/theme-provider'
+
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
@@ -22,8 +24,12 @@ export default function RootLayout({
         baseTheme: neobrutalism,
       }}
     >
-      <html lang="en">
-        <body className={inter.className}>{children}</body>
+      <html lang="en" suppressHydrationWarning>
+        <body className={inter.className}>
+          <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+            {children}
+          </ThemeProvider>
+        </body>
       </html>
     </ClerkProvider>
   )
