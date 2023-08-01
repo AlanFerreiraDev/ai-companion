aa4f0891196ed80b1656da86777597e79c9090d0
Alan Bruno Ferreira da Silva <alan.ferreira@pixter.com.br>
2023-08-01 20:05:30 -0300

    feat: add sidebar component

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -8,14 +8,15 @@ import { Button } from '@/components/ui/button'
 
 import { cn } from '@/lib/utils'
 import { ModeToggle } from '@/components/mode-toggle'
+import { MobileSidebar } from '@/components/mobile-sidebar'
 
 const font = Poppins({ weight: '600', subsets: ['latin'] })
 
 export const Navbar = () => {
   return (
-    <div className="fixed w-full z-50 flex justify-between items-center py-2 px-4 border-b border-primary/10 bg-secondary">
+    <div className="fixed w-full z-50 flex justify-between items-center py-2 px-4 border-b border-primary/10 bg-secondary h-16">
       <div className="flex items-center">
-        <Menu className="block md:hidden" />
+        <MobileSidebar />
         <Link href="/">
           <h1
             className={cn(
